eb4024245d47ecf5d70149cfe93ec71c10055880
Mashiro Wang <hyperoot@gmail.com>
2019-11-01 14:12:15 +0800

    fix: 修复删除旧插件标签的逻辑

--- src/background.ts
+++ src/background.ts
@@ -1,9 +1,13 @@
-chrome.tabs.onCreated.addListener(() => {
-  chrome.tabs.query({ title: 'RssTab', active: false }, oldTabs => {
-    oldTabs.forEach(tab => {
-      if (tab.id != null) {
-        chrome.tabs.remove(tab.id);
-      }
+chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
+  if (changeInfo.title === 'RssTab') {
+    chrome.tabs.query({ title: 'RssTab' }, oldTabs => {
+      oldTabs
+        .filter(tab => tab.id !== tabId)
+        .forEach(tab => {
+          if (tab.id != null) {
+            chrome.tabs.remove(tab.id);
+          }
+        });
     });
-  });
+  }
 });
--- src/store/constant/reducer.ts
+++ src/store/constant/reducer.ts
@@ -48,7 +48,7 @@ const initialState: ConstantState = {
       pattern: 'https://juejin.im/search?query=#REPLACE#&type=all',
     },
   ],
-  version: '1.1.2',
+  version: '1.1.3',
   homepage: 'https://github.com/mashirowang/rsstab',
 };
 
